297cd22400527cabd83813d9d265a9ef9418fa91
mhamza-dev <hamzarakha786@gmail.com>
2024-05-01 16:53:07 +0500

    Fiter games by genre

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,11 @@ import { Grid, GridItem, Show } from "@chakra-ui/react";
 import NavBar from "./components/NavBar";
 import GamesGrid from "./components/GamesGrid";
 import GenreList from "./components/GenreList";
+import { useState } from "react";
+import { Genre } from "./hooks/useGenres";
 
 function App() {
+  const [selectedGenre, SetSelectedGenre] = useState<Genre | null>(null);
   return (
     <>
       <Grid
@@ -11,20 +14,21 @@ function App() {
           base: `"nav" "main"`,
           lg: `"nav nav" "aside main"`,
         }}
-
         templateColumns={{
           base: "1fr",
-          lg: "200px 1fr"
+          lg: "200px 1fr",
         }}
       >
         <GridItem area={"nav"}>
           <NavBar />
         </GridItem>
         <Show above="lg">
-          <GridItem area={"aside"}><GenreList /> </GridItem>
+          <GridItem area={"aside"}>
+            <GenreList onSelectedGenre={(genre) => SetSelectedGenre(genre)} />{" "}
+          </GridItem>
         </Show>
         <GridItem area={"main"}>
-          <GamesGrid />
+          <GamesGrid selectedGenre={selectedGenre} />
         </GridItem>
       </Grid>
     </>
--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,18 +1,32 @@
-import { HStack, Image, List, ListItem, Spinner, Text } from "@chakra-ui/react";
-import useGenres from "../hooks/useGenres";
+import { Button, HStack, Image, List, ListItem, Spinner } from "@chakra-ui/react";
+import useGenres, { Genre } from "../hooks/useGenres";
 import resiseImgeUrl from "../services/reziseImage";
 
-const GenreList = () => {
-  const { data: genres, isLoading, error} = useGenres();
-  if (error) return null
+interface Props{
+   onSelectedGenre: (genre: Genre) => void;
+}
+
+const GenreList = ({ onSelectedGenre }: Props) => {
+  const { data: genres, isLoading, error } = useGenres();
+  if (error) return null;
   if (isLoading) return <Spinner />;
   return (
     <List paddingY={"5px"}>
-      {genres.map((gen) => (
-        <ListItem key={gen.id}>
+      {genres.map((genre) => (
+        <ListItem key={genre.id}>
           <HStack>
-            <Image boxSize={"32px"} borderRadius={8} src={resiseImgeUrl(gen.image_background)} />
-            <Text fontSize={"lg"}>{gen.name}</Text>
+            <Image
+              boxSize={"32px"}
+              borderRadius={8}
+              src={resiseImgeUrl(genre.image_background)}
+            />
+            <Button
+              onClick={() => onSelectedGenre(genre)}
+              fontSize={"lg"}
+              variant={"link"}
+            >
+              {genre.name}
+            </Button>
           </HStack>
         </ListItem>
       ))}
