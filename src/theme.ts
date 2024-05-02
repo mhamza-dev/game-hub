8f19e2204303b295ccf94750afe2e8c29e8fe725
mhamza-dev <hamzarakha786@gmail.com>
2024-05-02 12:22:09 +0500

    Customize the theme to get darker

--- src/theme.ts
+++ src/theme.ts
@@ -1,9 +1,25 @@
 import { ThemeConfig, extendTheme } from "@chakra-ui/react";
 
 const config: ThemeConfig = {
-    initialColorMode: "dark"
-}
+  initialColorMode: "dark",
+};
 
-const theme = extendTheme({ config  });
+const theme = extendTheme({
+  config,
+  colors: {
+    gray: {
+      50: "#f9f9f9",
+      100: "#ededed",
+      200: "#d3d3d3",
+      300: "#b363b3",
+      400: "#a0a0a0",
+      500: "#898989",
+      600: "#6c6c6с",
+      700: "#202020",
+      800: "#121212",
+      900: "#111",
+    },
+  },
+});
 
-export default theme
+export default theme;
