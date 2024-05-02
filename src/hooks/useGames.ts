aa826cfe0b4a3cb46371660bc98a18593734fa58
mhamza-dev <hamzarakha786@gmail.com>
2024-05-02 11:10:22 +0500

    Implemented searching

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -1,5 +1,5 @@
 import { GameQuery } from "../App";
-import useData from "./useData";;
+import useData from "./useData";
 import { Platform } from "./usePlatforms";
 
 export interface Game {
@@ -16,7 +16,8 @@ const useGames = (gameQuery: GameQuery) =>
       params: {
         genres: gameQuery.genre?.id,
         platforms: gameQuery.platform?.id,
-        ordering: gameQuery.sortOrder
+        ordering: gameQuery.sortOrder,
+        search: gameQuery.searchText,
       },
     },
     [gameQuery]
