41e798e9abf33f53115336d184ff4e123d8fa8f9
mhamza-dev <hamzarakha786@gmail.com>
2024-05-02 12:17:35 +0500

    replaced with static genres and platforms

---
+++ src/data/genres.ts
@@ -0,0 +1,154 @@
+export default [
+  {
+    id: 4,
+    name: "Action",
+    slug: "action",
+    games_count: 179665,
+    image_background:
+      "https://media.rawg.io/media/games/511/5118aff5091cb3efec399c808f8c598f.jpg",
+  },
+  {
+    id: 51,
+    name: "Indie",
+    slug: "indie",
+    games_count: 64558,
+    image_background:
+      "https://media.rawg.io/media/games/b6b/b6b20bfc4b34e312dbc8aac53c95a348.jpg",
+  },
+  {
+    id: 3,
+    name: "Adventure",
+    slug: "adventure",
+    games_count: 139281,
+    image_background:
+      "https://media.rawg.io/media/games/4e6/4e6e8e7f50c237d76f38f3c885dae3d2.jpg",
+  },
+  {
+    id: 5,
+    name: "RPG",
+    slug: "role-playing-games-rpg",
+    games_count: 55782,
+    image_background:
+      "https://media.rawg.io/media/games/ee3/ee3e10193aafc3230ba1cae426967d10.jpg",
+  },
+  {
+    id: 10,
+    name: "Strategy",
+    slug: "strategy",
+    games_count: 56096,
+    image_background:
+      "https://media.rawg.io/media/games/e40/e40cc9d1957b0a0ed7e389834457b524.jpg",
+  },
+  {
+    id: 2,
+    name: "Shooter",
+    slug: "shooter",
+    games_count: 59477,
+    image_background:
+      "https://media.rawg.io/media/games/1bd/1bd2657b81eb0c99338120ad444b24ff.jpg",
+  },
+  {
+    id: 40,
+    name: "Casual",
+    slug: "casual",
+    games_count: 52802,
+    image_background:
+      "https://media.rawg.io/media/games/a44/a444a7628bdb49b24d06a7672f805814.jpg",
+  },
+  {
+    id: 14,
+    name: "Simulation",
+    slug: "simulation",
+    games_count: 69286,
+    image_background:
+      "https://media.rawg.io/media/games/a88/a886c37bf112d009e318b106db9d420a.jpg",
+  },
+  {
+    id: 7,
+    name: "Puzzle",
+    slug: "puzzle",
+    games_count: 97274,
+    image_background:
+      "https://media.rawg.io/media/games/8cd/8cd179c85bd3de8f79bef245b15075fb.jpg",
+  },
+  {
+    id: 11,
+    name: "Arcade",
+    slug: "arcade",
+    games_count: 22656,
+    image_background:
+      "https://media.rawg.io/media/screenshots/6fe/6fe228662a253cd929cc78a103541ee0.jpg",
+  },
+  {
+    id: 83,
+    name: "Platformer",
+    slug: "platformer",
+    games_count: 100777,
+    image_background:
+      "https://media.rawg.io/media/screenshots/c97/c97b943741f5fbc936fe054d9d58851d.jpg",
+  },
+  {
+    id: 1,
+    name: "Racing",
+    slug: "racing",
+    games_count: 24705,
+    image_background:
+      "https://media.rawg.io/media/games/82e/82eeea65ebb047cc7f366faf2fb062b6.jpg",
+  },
+  {
+    id: 59,
+    name: "Massively Multiplayer",
+    slug: "massively-multiplayer",
+    games_count: 3607,
+    image_background:
+      "https://media.rawg.io/media/games/d0f/d0f91fe1d92332147e5db74e207cfc7a.jpg",
+  },
+  {
+    id: 15,
+    name: "Sports",
+    slug: "sports",
+    games_count: 21386,
+    image_background:
+      "https://media.rawg.io/media/games/ff6/ff66ce127716df74175961831ad3a23a.jpg",
+  },
+  {
+    id: 6,
+    name: "Fighting",
+    slug: "fighting",
+    games_count: 11751,
+    image_background:
+      "https://media.rawg.io/media/games/fbb/fbbd9fe21317bde9134114e2b1306069.jpg",
+  },
+  {
+    id: 19,
+    name: "Family",
+    slug: "family",
+    games_count: 5395,
+    image_background:
+      "https://media.rawg.io/media/games/793/79307648580a262f6cac402c3007efe0.jpg",
+  },
+  {
+    id: 28,
+    name: "Board Games",
+    slug: "board-games",
+    games_count: 8373,
+    image_background:
+      "https://media.rawg.io/media/screenshots/8ff/8ffe8f19d2e764867c8ed625ddf4e368.jpg",
+  },
+  {
+    id: 34,
+    name: "Educational",
+    slug: "educational",
+    games_count: 15675,
+    image_background:
+      "https://media.rawg.io/media/screenshots/6cd/6cd13ed3dcb6b44b8bc995850f2861e6.jpg",
+  },
+  {
+    id: 17,
+    name: "Card",
+    slug: "card",
+    games_count: 4526,
+    image_background:
+      "https://media.rawg.io/media/games/431/4317e294e88e4c9d77327693b15f499a.jpg",
+  },
+];
